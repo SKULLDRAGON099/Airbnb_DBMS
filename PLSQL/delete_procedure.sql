9be5b524a734df66dd4f6a17edae044c8b272359
Aastik Yadav <81007844+SKULLDRAGON099@users.noreply.github.com>
2022-05-20 04:39:32 +0530

    Update delete_procedure.sql

--- PLSQL/delete_procedure.sql
+++ PLSQL/delete_procedure.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE PROCEDURE delete_host_id(ID IN INTEGER)
 AS
 BEGIN
-      DELETE FROM hosts_entry WHERE hosts_id = ID;
+      DELETE FROM hosts_entry WHERE hosts_id = 1;
       dbms_output.put_line('Record Deleted');
 END;
